8d7e7b1aa9d8b3a53e6e0765ed6dcd32fd0651bc
N UMESH <102941752+Umesh8142@users.noreply.github.com>
2023-10-19 11:44:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+
+let browserName = navigator.appName;
+        
+        // Get the browser version
+let browserVersion = navigator.appVersion;
+
+const d=document.createElement('div');
+d.innerText=`You are using ${browserName} version ${browserVersion}`
+
+document.body.appendChild(d);
